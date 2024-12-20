0c4de90d971e084ac4c3ee3fa432231074828fd0
Seung-woo Seo <sw32.seo@gmail.com>
2024-12-21 03:56:56 +0900

    Remove Dev Container configuration and add QuizGPT Turbo application with file upload and Wikipedia search functionality

---
+++ quiz_gpt.py
@@ -0,0 +1,237 @@
+import os
+import json
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.retrievers import WikipediaRetriever
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.schema import BaseOutputParser
+
+
+class JsonOutputParser(BaseOutputParser):
+
+    def parse(self, text):
+        text = text.replace("```", "").replace("json", "")
+        return json.loads(text)
+
+
+output_parser = JsonOutputParser()
+
+# create a folders for the cache
+os.makedirs("./.cache", exist_ok=True)
+os.makedirs("./.cache/quiz_files", exist_ok=True)
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+@st.cache_resource(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    splitter = CharacterTextSplitter(
+        chunk_size=600,
+        chunk_overlap=100,
+        separator="\n",
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+
+@st.cache_data(show_spinner="Searching Wikipedia...")
+def wiki_search(topic):
+    retriever = WikipediaRetriever(top_k_results=2)
+    docs = retriever.get_relevant_documents(topic)
+    return docs
+
+
+questions_prompt = ChatPromptTemplate.from_template("""
+    You are a helpful assistant that is role playing as a teacher.
+
+    Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+
+    Use (o) to signal the correct answer.
+
+    Question examples:
+
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+
+    Your turn!
+
+    Context: {context}
+""")
+
+formatting_prompt = ChatPromptTemplate.from_template("""
+    You are a powerful formatting algorithm.
+
+    You format exam questions into JSON format.
+    Answers with (o) are the correct ones.
+
+    Example Input:
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+
+    Example Output:
+
+    ```json
+    {{ "questions": [
+            {{
+                "question": "What is the color of the ocean?",
+                "answers": [
+                        {{
+                            "answer": "Red",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Yellow",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Green",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Blue",
+                            "correct": true
+                        }},
+                ]
+            }},
+                        {{
+                "question": "What is the capital or Georgia?",
+                "answers": [
+                        {{
+                            "answer": "Baku",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Tbilisi",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Manila",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Beirut",
+                            "correct": false
+                        }},
+                ]
+            }},
+                        {{
+                "question": "When was Avatar released?",
+                "answers": [
+                        {{
+                            "answer": "2007",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2001",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2009",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "1998",
+                            "correct": false
+                        }},
+                ]
+            }},
+            {{
+                "question": "Who was Julius Caesar?",
+                "answers": [
+                        {{
+                            "answer": "A Roman Emperor",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Painter",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Actor",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Model",
+                            "correct": false
+                        }},
+                ]
+            }}
+        ]
+     }}
+    ```
+    Your turn!
+    Questions: {context}
+""")
+
+st.set_page_config(
+    page_title="QuizGPT Turbo",
+    page_icon=":question:",
+)
+
+st.title("QuizGPT Turbo")
+
+with st.sidebar:
+    # Get the API key from the user in the sidebar
+    api_key = st.text_input('Put your OpenAI API Key')
+
+    # Make the user choose between a file and a Wikipedia article
+    docs = None
+    choice = st.selectbox("Choose what you want to use.",
+                          ("File", "Wikipedia Article"))
+
+    if choice == "File":
+        file = st.file_uploader("Upload a file in .docx, .txt or .pdf format.",
+                                type=["docx", "txt", "pdf"])
+        if file:
+            docs = split_file(file)
+    else:
+        topic = st.text_input("Enter a topic.")
+        if topic:
+            docs = wiki_search(topic)
+
+    app_link = "https://github.com/sw32-seo/nomad_gpt_challenge/blob/main/quiz_gpt.py"
+    st.markdown("""
+    [Github](%s)
+    """ % app_link)
+
+if not docs:
+    st.markdown("""
+    Welcome to QuizGPT Turbo.
+
+    I will make a quiz from Wikipedia articles or files you upload to test your knowledge and help you study.
+
+    Get started by uploading a file or searching on Wikipedia in the sidebar.
+
+    Then you can set the dificulty level of the quiz.
+""")
